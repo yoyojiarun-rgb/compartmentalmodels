9c43d782d5f0d961aa9b09ad3a4e27a9d93ecb7f
Avery Yu <yoyojiarun@gmail.com>
2025-10-25 17:43:00 -0700

    Update SIRS parameter estimation.R

--- Estimation/SIRS parameter estimation.R
+++ Estimation/SIRS parameter estimation.R
@@ -1,10 +1,11 @@
 library(readxl)
-simdata <- read_excel("C:/Users/yoyoj/Downloads/alpha_0.1_data_SIRS.xlsx")
+simdata <- read_excel("C:/data_SIRS.xlsx")
 simdata$S.delta<- c(simdata$S[-1],0)-simdata$S
 simdata$I.delta<- c(simdata$I[-1],0)-simdata$I
 simdata$R.delta<- c(simdata$R[-1],0)-simdata$R
 simdata<- simdata[-nrow(simdata),]
 
+# estimating using regression
 for (j in 1:nrow(simdata)) {
   simdata$response.var[j] <- simdata$I.delta[j] / simdata$I[j]
 }
@@ -14,8 +15,7 @@ reg.summary <- summary(reg)
 print(beta.hatR<- N*reg.summary$coefficients[2,1])
 print(gamma.hatR_plus_alpha.hatR <- -reg.summary$coefficients[1,1])
 
-#method of moments
-
+# estimating using method of moments (both methods for alpha)
 
 for (j in 1:nrow(simdata)) {
   simdata$gamma.est[j]<- simdata$R.delta[j]/simdata$I[j]
